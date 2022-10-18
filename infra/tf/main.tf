23956ad875579ceabad36cfcd6e2caef65faa03b
kai.zhao <906942774@qq.com>
2022-10-18 22:53:37 +0800

    [kai.zhao] update docker file & docker terraform

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -11,16 +11,14 @@ provider "docker" {
   host    = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
+resource "docker_image" "data-node" {
+  name = "data-node"
+  build {
+    path = "../node"
+    tag  = ["data-node:1.0"]
+    label = {
+      author : "kai.zhao"
+    }
   }
 }
+
